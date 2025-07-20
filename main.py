4ad7abac1a05872696c3b5b606e09253a829a228
rupan100 <sankardaz01@gmail.com>
2025-07-20 18:21:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ from flask import Flask, request
 from telegram import Bot, Update
 from telegram.ext import Dispatcher, CommandHandler
 
-TOKEN = "7893622203:AAGEu1OQ1TjuRegqFprAtKTQLSeuJb0hY0Q"
+TOKEN = '7893622203:AAGEu1OQ1TjuRegqFprAtKTQLSeuJb0hY0Q'
 OWNER_ID = 7728185213
 
-bot = Bot(token=TOKEN)
 app = Flask(__name__)
+bot = Bot(token=TOKEN)
 
-dispatcher = Dispatcher(bot=bot, update_queue=None, workers=0, use_context=True)
+dispatcher = Dispatcher(bot=bot, update_queue=None, use_context=True)
 
 def start(update, context):
-    if update.effective_user.id == OWNER_ID:
-        update.message.reply_text("✅ Bot is Working!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="🤖 বট কাজ করছে!")
 
 dispatcher.add_handler(CommandHandler("start", start))
 
-@app.route("/", methods=["POST", "GET"])
+@app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
-    if request.method == "POST":
-        update = Update.de_json(request.get_json(force=True), bot)
-        dispatcher.process_update(update)
-    return "Working!", 200
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+    return 'OK'
+
+@app.route('/')
+def index():
+    return '✅ Bot is running.'
 
 if __name__ == '__main__':
     app.run()
